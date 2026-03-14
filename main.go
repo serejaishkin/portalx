e09ab24a9fce41596d9df48bc5d45be70a11c9a5
serejaishkin <47511319+serejaishkin@users.noreply.github.com>
2026-03-14 18:03:35 +0300

    Добавлен main.go с GUI на Fyne и обновлён go.mod

--- main.go
+++ main.go
@@ -1,68 +1,21 @@
 package main
 
 import (
-	"portalx/core"
-
-	"fyne.io/fyne/v2/app"
-	"fyne.io/fyne/v2/container"
-	"fyne.io/fyne/v2/widget"
+    "fyne.io/fyne/v2/app"
+    "fyne.io/fyne/v2/container"
+    "fyne.io/fyne/v2/widget"
 )
 
 func main() {
+    myApp := app.New()
+    w := myApp.NewWindow("PortalX GUI")
 
-	a := app.New()
-	w := a.NewWindow("PortalX")
+    w.SetContent(container.NewVBox(
+        widget.NewLabel("Добро пожаловать в PortalX!"),
+        widget.NewButton("Закрыть", func() {
+            myApp.Quit()
+        }),
+    ))
 
-	input := widget.NewEntry()
-	input.SetPlaceHolder("Paste VLESS or WG link")
-
-	status := widget.NewLabel("Disconnected")
-
-	importBtn := widget.NewButton("Import", func() {
-
-		err := core.ImportLink(input.Text)
-
-		if err != nil {
-			status.SetText("Import error")
-			return
-		}
-
-		status.SetText("Profile imported")
-	})
-
-	connectBtn := widget.NewButton("Connect", func() {
-
-		err := core.StartVPN()
-
-		if err != nil {
-			status.SetText("Connect error")
-			return
-		}
-
-		status.SetText("Connected")
-	})
-
-	disconnectBtn := widget.NewButton("Disconnect", func() {
-
-		err := core.StopVPN()
-
-		if err != nil {
-			status.SetText("Disconnect error")
-			return
-		}
-
-		status.SetText("Disconnected")
-	})
-
-	w.SetContent(container.NewVBox(
-		widget.NewLabel("PortalX VPN"),
-		input,
-		importBtn,
-		connectBtn,
-		disconnectBtn,
-		status,
-	))
-
-	w.Resize(fyne.NewSize(400, 260))
-	w.ShowAndRun()
+    w.ShowAndRun()
 }
