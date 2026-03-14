5dbe39aa9fd907e68076a52d37d1085fbd0418f1
serejaishkin <47511319+serejaishkin@users.noreply.github.com>
2026-03-14 23:49:02 +0300

    add vpn gui

--- main.go
+++ main.go
@@ -1,21 +1,44 @@
 package main
 
 import (
-    "fyne.io/fyne/v2/app"
-    "fyne.io/fyne/v2/container"
-    "fyne.io/fyne/v2/widget"
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/widget"
 )
 
 func main() {
-    myApp := app.New()
-    w := myApp.NewWindow("PortalX GUI")
 
-    w.SetContent(container.NewVBox(
-        widget.NewLabel("Добро пожаловать в PortalX!"),
-        widget.NewButton("Закрыть", func() {
-            myApp.Quit()
-        }),
-    ))
+	a := app.New()
+	w := a.NewWindow("PortalX")
 
-    w.ShowAndRun()
+	linkInput := widget.NewEntry()
+	linkInput.SetPlaceHolder("Вставьте VLESS / VPN ссылку")
+
+	status := widget.NewLabel("Статус: отключено")
+
+	importBtn := widget.NewButton("Импорт", func() {
+		status.SetText("Профиль импортирован")
+	})
+
+	connectBtn := widget.NewButton("Подключиться", func() {
+		status.SetText("Подключено")
+	})
+
+	disconnectBtn := widget.NewButton("Отключиться", func() {
+		status.SetText("Отключено")
+	})
+
+	content := container.NewVBox(
+		widget.NewLabel("PortalX VPN"),
+		linkInput,
+		importBtn,
+		connectBtn,
+		disconnectBtn,
+		status,
+	)
+
+	w.SetContent(content)
+	w.Resize(fyne.NewSize(420, 300))
+	w.ShowAndRun()
 }
